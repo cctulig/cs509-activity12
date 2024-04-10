b9a4ec3d61a37e321e6d506899f137b64bd88cc5
cctulig <cctulig@gmail.com>
2024-04-09 23:12:17 -0400

    Basic Math

---
+++ HandsOn12/HandsOn12/BasicMath.cs
@@ -0,0 +1,21 @@
+﻿namespace StringLibrary;
+
+/// <summary>
+/// Class <c>BasicMath</c> contains basic math functions
+/// </summary>
+public class BasicMath
+{
+    /// <summary>
+    /// Method <c>add</c> Adds two doubles and returns the result
+    /// </summary>
+    static public double add(double x, double y)
+    {
+        var a = someOtherFunction(x);
+        return a + y;
+    }
+
+    static private double someOtherFunction(double a)
+    {
+        return a;
+    }
+}
---
+++ HandsOn12/HandsOn12/Program.cs
@@ -0,0 +1,11 @@
+﻿using StringLibrary;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Adding 1 and 2:");
+        Console.WriteLine(BasicMath.add(1, 2));
+    }
+}
